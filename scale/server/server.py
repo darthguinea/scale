ed5819ec5737c4d1888b465776d71d7686c07786
darthguinea <darthguinea@protonmail.com>
2016-12-22 14:44:29 +1100

    Some basic changes to the server.py, params are not working

--- scale/server/server.py
+++ scale/server/server.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 from scale.config import Config
 from scale.utilities.tags import Tags
 from scale.utilities.user_data import UserData
@@ -8,7 +8,7 @@ class Server(Config):
     def __init__(self, ec2_environment='default',
                             name=None,
                             dry_run=False,
-                            ami='ami-06116566',
+                            ami='ami-d8bdebb8',
                             environment='stage',
                             chef_role=None,
                             tags=[],
@@ -20,11 +20,11 @@ class Server(Config):
                             security_group=None,
                             keypair='~/.ssh/stage.pem',
                             ):
-<<<<<<< HEAD
         self.name = name
         self.dry_run = dry_run
         self.ami = ami
         self.environment = environment
+        self.chef_role = chef_role
         self.availability_zone = availability_zone
         self.instance_type = instance_type
         self.disks = disks
@@ -32,28 +32,18 @@ class Server(Config):
         self.user_data = user_data
         self.security_group_ids = security_group_ids
         self.keypair = keypair
-=======
-      self.name = name
-      self.dry_run = dry_run
-      self.ami = ami
-      self.environment = environment
-      self.chef_role = chef_role
-      self.availability_zone = availability_zone
-      self.security_group = security_group
-      self.instance_type = instance_type
-      self.disks = disks
-      self.tags = tags
-      self.user_data = user_data
-      self.keypair = keypair
->>>>>>> AddTagObject
+
+        self.configure()
 
         super(Server, self).__init__(ec2_environment=ec2_environment)
 
+
     def configure(self):
+        if self.availability_zone is None:
+            self.availability_zone = random.sample(['a', 'b', 'c'], 1)[0]
         
 
     def bake(self):
-<<<<<<< HEAD
         self.log.info('Starting server build')
 
         try:
@@ -68,8 +58,8 @@ class Server(Config):
                 'MaxCount': 1,
                 'Placement': {
                     'AvailabilityZone': '{region}{az}'.format(region=self.region,
-                    az=self.availability_zone)
-                    }
+                                                                az=self.availability_zone)
+                }
             }
 
             ec2 = self.session.resource('ec2')
@@ -80,41 +70,4 @@ class Server(Config):
 
         except Exception as e:
             self.log.error('Did not create instance due to [{e}]'.format(e=e))
-=======
-
-      self.log.info('Starting server build')
-
-
-      if (self.tags) < 1:
-        self.log.info('No tags defined, creating a basic list')
-        self.tags.append('Name', self.name)
-        self.tags.append('Environment', self.environment)
-        self.tags.append('Role', self.chef_role)
-
-
-      try:
-        params = {
-              'DryRun': self.dry_run,
-              'ImageId': self.ami,
-              'InstanceType': self.instance_type,
-              'KeyName': self.keypair,
-              'UserData': self.user_data,
-              'SecurityGroupIds': self.security_group_ids,
-              'MinCount': 1,
-              'MaxCount': 1,
-              'SubnetId': self.subnet,
-              'Placement': {
-                  'AvailabilityZone': '{region}{az}'.format(region=self.region,
-                                          az=self.availability_zone)
-                  }
-              }
-        instances = ec2.create_instances(**params)
-
-        for i in instances:
-          i.create_tags(Tags=self.tags)
-
-      except Exception as e:
-
-        self.log.error('Did not create instance due to [{e}]'.format(e=e))
->>>>>>> AddTagObject
 
