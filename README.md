5b8ea0deef161137eb8f1abe03c0420ef2f4fec5
darthguinea <darthguinea@protonmail.com>
2016-12-30 14:46:21 +1100

    Updated readme file and now you can delete rules from security groups

--- README.md
+++ README.md
@@ -4,7 +4,20 @@ Use this applcation to deploy new EC2 servers
 ##Install
 
 ```
+#Once downloaded, in the scale folder using terminal run:
+
 python setup.py install
+
+
+#Create an .py file (in this case, example.py):
+
+cat >> example.py << EOF
+from scale.server.server import Server
+
+Server(keypair='stage', ec2_environment='default', region='us-west-1', name='my_awesome_server').create()
+EOF
+
+python example.py
 ```
 
 ##How to use, basic server build:
@@ -28,4 +41,18 @@ my_tags.add('environment', 'stage')
 Server(keypair='stage', ec2_environment='default', region='us-west-1', tags=my_tags.get()).create()
 ```
 
+##Security Groups:
+```
+from scale.server.server import Server
+from scale.networking.security_group import SecurityGroup
 
+
+rules = [{"IP": "10.0.2.1/32", 'FromPort': "80", 'ToPort': "80", 'Protocol': "tcp"},
+        {"IP": "172.16.32.1/32", 'FromPort': "80", 'ToPort': "80", 'Protocol': "tcp"}]
+
+
+my_sg_id = SecurityGroup(name='p-web', region='us-west-1', description='web server sg', rules=rules).create()
+
+Server(keypair='stage', security_groups=[my_sg_id], ec2_environment='default', region='us-west-1', name='my_awesome_server').create()
+
+```
--- scale/networking/security_group.py
+++ scale/networking/security_group.py
@@ -5,6 +5,7 @@ class SecurityGroup(Config):
     def __init__(self, 
                     ec2_environment='default',
                     region='us-east-1',
+                    group_id=None,
                     name=None, 
                     description=None,
                     vpc_id=None,
@@ -12,12 +13,14 @@ class SecurityGroup(Config):
 
         self.ec2_environment = ec2_environment
         self.region = region
+        self.group_id = group_id
         self.name = name
         self.description = description
         self.vpc_id = vpc_id
         self.rules = rules
 
         super(SecurityGroup, self).__init__(ec2_environment=self.ec2_environment, region=self.region)
+
 
     def create(self):
         if self.name is None:
@@ -37,21 +40,23 @@ class SecurityGroup(Config):
 
         try:
             response = ec2.create_security_group(**params)
+            group_id = response.id
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == 'InvalidGroup.Duplicate':
-                self.log.error('Security Group [{sg}] already exists!'.format(sg=self.name))
-            return
+                self.log.error('Security Group [{sg}] already exists! '\
+                                'I will attempt to update the rules'.format(sg=self.name))
+                group_id = self.get_existing_sg_id()
 
         if len(self.rules) > 0:
             for rule in self.rules:
-                self.add(sg=response.id, ip=rule['IP'], from_port=rule['FromPort'],
+                self.add(group_id=group_id, ip=rule['IP'], from_port=rule['FromPort'],
                                 to_port=rule['ToPort'], protocol=rule['Protocol'])
 
-        return response.id
+        return group_id
 
-    def add(self, sg, ip, from_port, to_port, protocol):
+    def add(self, group_id, ip, from_port, to_port, protocol):
         ec2 = self.session.resource('ec2')
-        security_group = ec2.SecurityGroup(sg)
+        security_group = ec2.SecurityGroup(group_id)
 
         params = {
             'IpProtocol': protocol,
@@ -60,6 +65,36 @@ class SecurityGroup(Config):
             'IpRanges': [{'CidrIp': ip}]
         }
 
-        security_group.authorize_ingress(GroupId=sg, IpPermissions=[params])
+        try:
+            security_group.authorize_ingress(GroupId=group_id, IpPermissions=[params])
+        except botocore.exceptions.ClientError as e:
+          if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
+            self.log.error('Security Group Rule for [{sg}] [{ip}] Already exists!'.format(sg=group_id, ip=ip))
 
 
+    def get_existing_sg_id(self, name=None):
+        # Fetch the security group ID
+        if name is None:
+            name = self.name
+        client = self.session.client('ec2')
+        response = client.describe_security_groups(GroupNames=[name])
+
+        return response['SecurityGroups'][0]['GroupId']
+
+
+    def delete_all(self, group_id):
+        ec2 = self.session.resource('ec2')
+        security_group = ec2.SecurityGroup(group_id)
+
+        client = self.session.client('ec2')
+        response = client.describe_security_groups(GroupIds=[group_id])
+
+        security_group.revoke_ingress(IpPermissions=response['SecurityGroups'][0]['IpPermissions'])
+
+
+    def delete(self, group_id=None, name=None):
+        if group_name is not None:
+            self.session.delete_security_group(GroupName=name)
+
+        if group_id is not None:
+            self.session.delete_security_group(GroupId=group_id)
