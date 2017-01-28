a120fa5ef9e44418a14ac291bbdce723d60b201d
darthguinea <darthguinea@protonmail.com>
2017-01-28 20:38:06 +1100

    Updated user data file

--- scale/utils/user_data.py
+++ scale/utils/user_data.py
@@ -1,46 +1,36 @@
 import os
+from scale.config import Config
 
 
-class UserData(object):
-    def __init__(self,
-                    user_data_file='ubuntu',
-                    org='my_org',
-                    environment='stage',
-                    enc_data_bag=None,
-                    validation_key=None,
-                    chef_url=None,
-                    chef_role=None):
+class UserData(Config):
 
-        self.user_data_file = user_data_file
-        self.org = org
-        self.environment = environment
-        self.enc_data_bag = enc_data_bag
-        self.validation_key = validation_key
-        self.chef_url = chef_url
-        self.chef_role = chef_role
+    def __init__(self):
+
+        super(UserData, self).__init__()
 
 
-    def get_enc_data_bag(self):
-        pass
+    def open(self, filename=None):
+        try:
+            root = os.path.expanduser(filename)
+            file_data = open(root, 'r').read()
+
+            self.log.info('Loading user data file [{0}]'.format(root))
+
+            return file_data
+        except:
+            self.log.error('Could not load user data file [{0}]'.format(filename))
+            return None
 
 
-    def get_validation_key(self):
-        pass
+    def create(self,
+                    user_file='ubuntu',
+                    params=None
+                    ):
 
-
-    def create(self):
         root = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(root, '..', '..', 'data', self.user_data_file)
+        path = os.path.join(root, '..', '..', 'data', user_file)
 
-        user_file = open(path, 'r').read()
-
-        print root
-        print path
-
-        print user_file.format(role=self.chef_role,
-                encrypted_data_bag=self.enc_data_bag,
-                validation_key=self.validation_key,
-                environment=self.environment,
-                chef_url=self.chef_url,
-                org=self.org
-                )
+        if params is None:
+            return open(path, 'r').read()
+        else:
+            return open(path, 'r').read().format(**params)
